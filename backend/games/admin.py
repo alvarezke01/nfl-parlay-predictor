fdc1105095578e3f49df73553cf216b0362e0a2b
kathrynalv <alvarezke01@gmail.com>
2025-09-10 01:14:15 -0400

    chore(games): register Team, Game, Odds in admin with list displays

--- backend/games/admin.py
+++ backend/games/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Team, Game, Odds
 
-# Register your models here.
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "conference", "division")
+    search_fields = ("name",)
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("id", "season", "week", "home_team", "away_team", "kickoff", "venue", "roof")
+    list_filter = ("season", "week", "venue", "roof")
+    search_fields = ("home_team__name", "away_team__name")
+    autocomplete_fields = ("home_team", "away_team")
+
+
+@admin.register(Odds)
+class OddsAdmin(admin.ModelAdmin):
+    list_display = ("id", "game", "book", "spread", "spread_price", "total", "total_price", "moneyline_home", "moneyline_away", "captured_at")
+    list_filter = ("book",)
+    search_fields = ("book", "game__home_team__name", "game__away_team__name")
